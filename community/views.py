0b80d1f4712029a66935a5ab1fc9cd93e122f224
BettK <kelvin.bett@student.moringaschool.com>
2021-09-24 21:47:41 +0300

    updated theviewspy

--- community/views.py
+++ community/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import *
+
+from .forms import *
+from decouple import config,Csv
+import datetime as dt
+from django.http import JsonResponse
+import json
+from django.db.models import Q
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Welcome to neighbout Hood application')
