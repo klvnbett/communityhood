e3115facdbd5233b3decb3e572f65b83baaab001
BettK <kelvin.bett@student.moringaschool.com>
2021-09-27 20:47:53 +0300

    registered models to admin py

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+admin.site.register(Neighbourhood)
+admin.site.register(Health,HealthAdmin)
+admin.site.register(Business)
+admin.site.register(Healthservices)
+admin.site.register(Authorities)
+admin.site.register(BlogPost)
+admin.site.register(Profile)
+admin.site.register(Notifications)
