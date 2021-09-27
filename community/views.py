40805b5bdf74bcad72319eddcef067efabfe8a56
BettK <kelvin.bett@student.moringaschool.com>
2021-09-27 07:33:22 +0300

    will add notification and blog views

--- community/views.py
+++ community/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from .models import *
 
-from .forms import *
+
 from decouple import config,Csv
 import datetime as dt
 from django.http import JsonResponse
@@ -16,3 +16,18 @@ from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     return HttpResponse(request,'hood/index.html')
+
+def blog(request):
+    current_user=request.user
+    profile=Profile.objects.get(username=current_user)
+    blogposts = BlogPost.objects.filter(neighbourhood=profile.neighbourhood)
+    return render(request,'hood/blog.html',{"blogposts":blogposts})
+
+
+def notification(request):
+    current_user=request.user
+    profile=Profile.objects.get(username=current_user)
+    all_notifications = notifications.objects.filter(neighbourhood=profile.neighbourhood)
+    
+
+    return render(request,'hood/notifications.html',{"notifications":all_notifications})
