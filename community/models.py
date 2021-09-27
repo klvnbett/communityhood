e0c2e72cb62e8c3a480b34f81fb1a035f7f337e4
BettK <kelvin.bett@student.moringaschool.com>
2021-09-27 07:52:36 +0300

    will add neighorhood and notifications" git commit -m"will add neighorhood and notifications"

--- community/models.py
+++ community/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models import Q
+import datetime as dt
 
 # Create your models here.
+
+
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+
+class notifications(models.Model):
+    title = models.CharField(max_length=100)
+    notification = HTMLField()
+    priority = models.CharField(max_length=15,choices=Priority,default="Informational")
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
