d8c9c8a2970fd3e01ff94d5c70293909d26b897f
BettK <kelvin.bett@student.moringaschool.com>
2021-09-28 14:45:33 +0300

    updated and mmodified models classes

--- community/models.py
+++ community/models.py
@@ -1,44 +1,53 @@
-from django.db import models
-from django.contrib.auth.models import User
-from tinymce.models import HTMLField
-from django.db.models import Q
-import datetime as dt
-
 Priority=(
     ('Informational','Informational'),
     ('High Priority','High Priority'),
 )
 
 # Create your models here.
-class Neighbourhood(models.Model):
+class neighbourhood(models.Model):
     neighbourhood= models.CharField(max_length=100)
 
     def __str__(self):
         return self.neighbourhood
 
-class Notifications(models.Model):
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
+
+
+class notifications(models.Model):
     title = models.CharField(max_length=100)
     notification = HTMLField()
     priority = models.CharField(max_length=15,choices=Priority,default="Informational")
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
 
-class Healthservices(models.Model):
+class healthservices(models.Model):
     healthservices = models.CharField(max_length=100)
 
     def __str__(self):
         return self.healthservices
 
+    def save_healthservices(self):
+        self.save()
+
+    @classmethod
+    def delete_healthservices(cls,healthservices):
+        cls.objects.filter(healthservices=healthservices).delete()
 
 
 class Business(models.Model):
     logo = models.ImageField(upload_to='businesslogo/')
     description = HTMLField()
-    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
@@ -50,18 +59,18 @@ class Business(models.Model):
 
 class Health(models.Model):
     logo = models.ImageField(upload_to='healthlogo/')
-    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
     contact = models.IntegerField()
     address =models.CharField(max_length=100)
-    healthservices = models.ManyToManyField(Healthservices)
+    healthservices = models.ManyToManyField(healthservices)
 
     def __str__(self):
         return self.name
 
 class Authorities(models.Model):
-    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
     contact = models.IntegerField()
@@ -74,7 +83,7 @@ class Authorities(models.Model):
 class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/')
     description = HTMLField()
-    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
@@ -87,10 +96,20 @@ class BlogPost(models.Model):
     image = models.ImageField(upload_to='post/')
     post = HTMLField()
     username = models.ForeignKey(User,on_delete=models.CASCADE)
-    neighbourhood= models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighbourhood= models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
     post_date = models.DateTimeField(auto_now_add=True)
     avatar = models.ImageField(upload_to='avatars/')
 
-
     def __str__(self):
         return self.title
+
+    @classmethod
+    def search_blogpost(cls,search_term):
+        blogs = cls.objects.filter(Q(username__username=search_term) | Q(neighbourhood__neighbourhood=search_term) | Q(title__icontains=search_term))
+        return blogs
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=300)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
