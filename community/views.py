a9c21fbd636f469e4a4a8c97f07851abfe09b0c3
BettK <kelvin.bett@student.moringaschool.com>
2021-09-28 16:01:34 +0300

    will update the app imports

--- community/views.py
+++ community/views.py
@@ -3,15 +3,17 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from .models import *
-
-
-
+from .email import send_priority_email
+from .forms import *
+from decouple import config,Csv
 import datetime as dt
 from django.http import JsonResponse
 import json
 from django.db.models import Q
 from django.contrib.auth.models import User
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 def index(request):
