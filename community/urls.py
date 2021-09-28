dc5070ded92cee3ba04b0ecf7c974cfd5728bce6
BettK <kelvin.bett@student.moringaschool.com>
2021-09-28 15:55:40 +0300

    will update the app urls pattern

--- community/urls.py
+++ community/urls.py
@@ -12,7 +12,12 @@ urlpatterns=[
     url(r'^businesses',views.businesses, name='businesses'),
     url(r'^view/blog/(\d+)',views.view_blog,name='view_blog'),
     url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
-    
+    url(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'),
+    url(r'^new/business$',views.new_business, name='new-business'),
+    url(r'^create/profile$',views.create_profile, name='create-profile'),
+    url(r'^new/notification$',views.new_notification, name='new-notification'),
+    url(r'^update/profile$',views.update_profile, name='update-profile'),
+    url(r'^search/',views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
