ce7df4b6144006d78b49b8af7f1ca0f938e69ff3
BettK <kelvin.bett@student.moringaschool.com>
2021-09-30 10:01:42 +0300

    heroku deployement

--- neihborhood/settings.py
+++ neihborhood/settings.py
@@ -12,6 +12,38 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +58,7 @@ SECRET_KEY = 'django-insecure-3)4n$kebm+$ajhlf+r0kqv-8jv!a=sz&t8&sg)%ok(u(lrb4ti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +78,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,19 +156,27 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR,'static')
 ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
+
+
+#Configure Django App for Heroku
+django_heroku.settings(locals())
---
+++ readme.md
@@ -0,0 +1,76 @@
+
+# COMMUNITY HOOD 
+## Author Kipkemoi Bett
+
+## Description
+This is a web application where users can post project(s) they have created and have them reviewed by their peers.
+
+------------------------------------------------------------------------
+
+## User Requirements
+
+1. Sign in to the application to start using.
+2. Post a project to be rated/reviewed.
+3. Rate/ review other users' projects.
+4. Search for projects.
+5. View projects overall score
+6. View my profile page.
+
+### Live Link ###
+
+## Getting started
+
+### Cloning the repository
+```bash
+git clone https://github.com/klvnbett/communityhood.git
+```
+
+### Creating a virtual environment
+
+```bash
+python3 -m virtualenv virtual
+source virtual/bin/activate
+```
+### Installing dependencies
+```bash
+pip3 install -r requirements
+```
+
+### Database migrations
+
+```bash
+python manage.py migrate
+```
+
+### Running the server
+```bash
+python3.6 manage.py runserver
+```
+
+### Admin Dashboard
+Use django admin to manage the different users and posts.
+
+
+## Running the tests
+```bash
+python manage.py app test
+```
+
+## Technology used
+
+* Python3.6
+* Django 
+* Heroku
+* Bootstrap
+
+
+## Known Bugs
+working on the testing part
+working on the profile link
+There are no known bugs.
+
+## License
+MIT License
+
+
+
